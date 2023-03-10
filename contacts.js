eda2db65291d5eb199659ca5c5094952e3ea726d
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2023-03-10 17:30:06 +0200

    Update contacts.js
    Added try catch to async functions

--- contacts.js
+++ contacts.js
@@ -9,9 +9,13 @@ const contactsPath = path.resolve("db", "contacts.json");
  * @returns {array} - list of contacts.
  */
 async function listContacts() {
-  const readResult = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(readResult);
-  return contacts;
+  try {
+    const readResult = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(readResult);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 /**
@@ -20,25 +24,35 @@ async function listContacts() {
  * @returns {object} contactById - contact found or null.
  */
 async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contactById = contacts.find((contact) => Number(contact.id) === contactId);
-  if (!contactById) {
-    return null;
+  try {
+    const contacts = listContacts();
+    const contactById = contacts.find(
+      (contact) => Number(contact.id) === contactId
+    );
+    if (!contactById) {
+      return null;
+    }
+    return contactById;
+  } catch (error) {
+    console.log(error);
   }
-  return contactById;
 }
 
 /**
  * Reading of contacts.json and adding new contact.
  * @param {string} name - name of the new contact.
  * @param {string} email - email of the new contact.
- * @param {string} phone - phone number of the new contact 
+ * @param {string} phone - phone number of the new contact
  */
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const newContact = { id: shortid.generate(), name, email, phone };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  try {
+    const contacts = listContacts();
+    const newContact = { id: shortid.generate(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 /**
@@ -47,14 +61,20 @@ async function addContact(name, email, phone) {
  * @returns {object} contacts[idx] - removed contact or null.
  */
 async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const idx = contacts.findIndex((contact) => Number(contact.id) === contactId);
-  if (idx === -1) {
-    return null;
+  try {
+    const contacts = listContacts();
+    const idx = contacts.findIndex(
+      (contact) => Number(contact.id) === contactId
+    );
+    if (idx === -1) {
+      return null;
+    }
+    const newContactsList = contacts.filter((_, index) => index !== idx);
+    await fs.writeFile(contactsPath, JSON.stringify(newContactsList));
+    return contacts[idx];
+  } catch (error) {
+    console.log(error);
   }
-  const newContactsList = contacts.filter((_, index) => index !== idx);
-  await fs.writeFile(contactsPath, JSON.stringify(newContactsList));
-  return contacts[idx];
 }
 
 module.exports = { listContacts, getContactById, addContact, removeContact };
